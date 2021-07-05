bd1b9eb97097bb04eea6ce4f1d1fa7d7f1a2a406
Ali-Enes-a <84739715+Ali-Enes-a@users.noreply.github.com>
2021-07-05 10:37:11 +0300

    Proje dosyası ekle.

---
+++ inidosyasiolusturucu/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace inidosyasiolusturucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.okubtn = new System.Windows.Forms.Button();
+            this.islembtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(626, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(370, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Değer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Değer Adı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Bölüm Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(168, 161);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(134, 63);
+            this.btnKaydet.TabIndex = 12;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // okubtn
+            // 
+            this.okubtn.Location = new System.Drawing.Point(390, 161);
+            this.okubtn.Name = "okubtn";
+            this.okubtn.Size = new System.Drawing.Size(123, 63);
+            this.okubtn.TabIndex = 13;
+            this.okubtn.Text = "Veri Oku";
+            this.okubtn.UseVisualStyleBackColor = true;
+            this.okubtn.Click += new System.EventHandler(this.okubtn_Click);
+            // 
+            // islembtn
+            // 
+            this.islembtn.Location = new System.Drawing.Point(275, 271);
+            this.islembtn.Name = "islembtn";
+            this.islembtn.Size = new System.Drawing.Size(141, 59);
+            this.islembtn.TabIndex = 14;
+            this.islembtn.Text = "İşlem";
+            this.islembtn.UseVisualStyleBackColor = true;
+            this.islembtn.Click += new System.EventHandler(this.islembtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.islembtn);
+            this.Controls.Add(this.okubtn);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button okubtn;
+        private System.Windows.Forms.Button islembtn;
+    }
+}
+
---
+++ inidosyasiolusturucu/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inidosyasiolusturucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class inikaydet
+        {
+            [DllImport("kernel32")]
+            private static extern long WritePrivateProfileString(string section, string key, string value, string filepath);
+            [DllImport("kernel32")]
+            private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filepath);
+
+
+            public inikaydet(string dosyayolu)
+            {
+                dosyol = dosyayolu;
+            }
+            private string dosyol = string.Empty;
+            public string varsayilan { get; set; }
+
+            public string Oku(string bolum, string ayaradi)
+            {
+                varsayilan = varsayilan ?? string.Empty;
+                StringBuilder strBuild = new StringBuilder(256);
+                GetPrivateProfileString(bolum, ayaradi, varsayilan, strBuild, 255, dosyol);
+                return strBuild.ToString();
+
+            }
+            public long yaz(string bolum, string ayaradi, string deger)
+            {
+                return WritePrivateProfileString(bolum, ayaradi, deger, dosyol);
+
+            }
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            inikaydet ini = new inikaydet(Application.StartupPath + @"\Ayarlar.ini");
+            ini.yaz(textBox1.Text, textBox2.Text, textBox3.Text);
+
+            MessageBox.Show("Ayarlar kaydedildi");
+        }
+
+        private void okubtn_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (File.Exists(Application.StartupPath + @"\Ayarlar.ini"))
+                {
+                    inikaydet ini = new inikaydet(Application.StartupPath + @"\Ayarlar.ini");
+
+                    textBox3.Text = ini.Oku(textBox1.Text, textBox2.Text);
+                }
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("ini dosyası hasarlı " + hata);
+            }
+            if (textBox3.Text == "")
+            {
+                inikaydet ini = new inikaydet(Application.StartupPath + @"\Ayarlar.ini");
+                ini.yaz(textBox1.Text, textBox2.Text, textBox3.Text);
+            }
+        }
+
+        private void islembtn_Click(object sender, EventArgs e)
+        {
+            inikaydet ini = new inikaydet(Application.StartupPath + @"\settings.ini");
+            if (File.Exists("credentials.txt") == true)
+            {
+                string[] lines = File.ReadAllLines("credentials.txt");
+
+
+                if (lines.Length > 0)
+                {
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+
+                            ini.yaz("sistem", "hotelid", lines[i]);
+                        }
+                        else if (i == 1)
+                        {
+
+                            ini.yaz("sistem", "username", lines[i]);
+                        }
+                        else if (i == 2)
+                        {
+
+                            ini.yaz("sistem", "password", lines[i]);
+                        }
+                    }
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 1; i++)
+                {
+
+
+                    ini.yaz("sistem", "hotelid", "");
+
+
+
+                    ini.yaz("sistem", "username", "");
+
+
+
+                    ini.yaz("sistem", "password", "");
+
+                }
+            }
+        }
+    }
+}
